61d23b169d5e08f40131d077cf1f53e4713ff07e
Justin Leach <web.justin.leach@gmail.com>
2015-04-17 22:26:15 -0500

    Updated name

---
+++ server/config.js
@@ -0,0 +1,19 @@
+/* 
+    Created on : Mar 29, 2015, 4:07:24 PM
+    Author     : Justin Leach
+*/
+
+'use strict';
+
+module.exports = {
+   db: 'localhost', 
+   clientSecret: process.env.clientSecret || 'b7c61d0cfc2545658b2dda5b23556ac3', 
+   tokenSecret: process.env.tokenSecret || 'stringHardGuess'
+}; 
+
+
+
+
+
+
+
